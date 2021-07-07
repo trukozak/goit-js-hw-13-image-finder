313d516479541c6483b8d43aea18b15b07e92c06
trukozak <aleksandr.nepijvoda@gmail.com>
2021-07-07 13:19:25 +0300

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,60 +1,65 @@
 import '../sass/main.scss';
-import debounce from 'lodash.debounce';
-import { error, defaultModules } from '@pnotify/core';
-import * as PNotifyMobile from '@pnotify/mobile/dist/PNotifyMobile.js';
+import { error } from '@pnotify/core';
 import '@pnotify/core/dist/PNotify.css';
 import '@pnotify/core/dist/BrightTheme.css';
 import 'material-design-icons/iconfont/material-icons.css';
 import * as basicLightbox from 'basiclightbox';
 import 'basiclightbox/dist/basicLightbox.min.css';
-defaultModules.set(PNotifyMobile, {});
 
 import { ApiService } from './apiService.js';
 import pictureCardTpl from '../tpl/pictureCard.hbs';
 
-const searchQueryRef = document.querySelector('input[name="query"]');
+const searchQueryRef = document.querySelector('.search-form');
 const pictureCardRef = document.querySelector('.gallery');
 const loadMoreBtn = document.querySelector('.load-more-btn');
 
 const newApiService = new ApiService();
 
-searchQueryRef.addEventListener('input', debounce(onInputSearch, 1000));
+searchQueryRef.addEventListener('submit', onSearch);
 loadMoreBtn.addEventListener('click', onLoadMoreClick);
 pictureCardRef.addEventListener('click', onOpenOriginalPicture);
 
-function onInputSearch(e) {
-  const input = e.target.value;
-  clearPictureCard();
-  newApiService.query = input;
+function onSearch(e) {
+  e.preventDefault();
 
-  if (newApiService.query === '') {
-    loadMoreBtn.classList.add('is-hidden');
-
-    error({
-      text: 'Please, enter a more specific query',
-      delay: 2000,
-    });
-    return;
-  }
-
+  newApiService.query = e.currentTarget.elements.query.value;
   newApiService.resetPage();
+  clearPictureCard();
+
   newApiService
     .fetchSearch()
     .then(renderPictureCard)
     .catch(error => error);
+
+  if (newApiService.query === '') {
+    error({
+      text: 'Please, enter a more specific query',
+      delay: 1000,
+    });
+  }
 }
 
 function renderPictureCard(hits) {
   loadMoreBtn.classList.remove('is-hidden');
   onMarkup(hits);
-  if (hits.length < 12) loadMoreBtn.classList.add('is-hidden');
+  if (hits.length < 12) {
+    loadMoreBtn.classList.add('is-hidden');
+  }
+
+  if (hits.length === 0) {
+    error({
+      text: 'Correct request',
+      delay: 1000,
+    });
+    return;
+  }
 
   setTimeout(() => {
     window.scrollTo({
       top: document.documentElement.scrollHeight,
       behavior: 'smooth',
     });
-  }, 1500);
+  }, 2000);
 }
 
 function clearPictureCard() {
@@ -63,6 +68,10 @@ function clearPictureCard() {
 
 function onMarkup(elem) {
   pictureCardRef.insertAdjacentHTML('beforeend', pictureCardTpl(elem));
+  if (newApiService.query === '') {
+    clearPictureCard();
+    loadMoreBtn.classList.add('is-hidden');
+  }
 }
 
 function onLoadMoreClick(e) {
@@ -72,7 +81,7 @@ function onLoadMoreClick(e) {
 function onOpenOriginalPicture(e) {
   if (e.target.tagName !== 'IMG') return;
 
-  const imgSrc = e.target.getAttribute('original-src');
+  const imgSrc = e.target.getAttribute('data-original-src');
   const instance = basicLightbox.create(`<img src="${imgSrc}" width="1000">`);
   instance.show();
 }
